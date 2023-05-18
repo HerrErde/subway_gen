ddaed40790463c459181c9f32976c975cea595d7
Herr Erde <me@herrerde.xyz>
2023-05-18 18:32:07 +0200

    adding Token Boost

--- generator/upgrades.php
+++ generator/upgrades.php
@@ -4,27 +4,23 @@ $required_params = [
   "superSneakers",
   "magnet",
   "doubleScore",
-  "doubleCoinsAmount"
+  "doubleCoinsAmount",
+  "tokenBoostAmount"
 ];
 
 $errors = [];
 
 foreach ($required_params as $param) {
-  if ($param === "doubleCoinsAmount" && empty($_GET[$param])) {
+  if (($param === "doubleCoinsAmount" || $param === "tokenBoostAmount") && empty($_GET[$param])) {
     $_GET[$param] = 1;
-  } else {
-    if (empty($_GET[$param])) {
-      $errors[] = "Failed to generate. Try again.";
-    }
+  } elseif (empty($_GET[$param])) {
+    $errors[] = "Failed to generate. Try again.";
   }
 }
 
 if (!empty($errors)) {
   $_SESSION["error"] = implode("<br>", $errors);
-  header("Location:../code/upgrades.php");
-  exit();
-}
-?>
+header("Location:../code/upgrades.php"); exit(); } ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,22 +28,26 @@ if (!empty($errors)) {
     <title>Code for the upgrades.json file</title>
     <?php require "../require/connect.php"; ?>
     <script src="../assets/js/script.js"></script>
-    <script>var filename = 'upgrades.json';</script>
+    <script>
+      var filename = 'upgrades.json';
+    </script>
   </head>
 
   <body>
     <header>
       <h1>Code for the upgrades.json file</h1>
       <p id="title">
-        Download or copy the generated code, find the file wallet.json in the folder
-        "profile" and paste it there.
+        Download or copy the generated code, find the file wallet.json in the
+        folder "profile" and paste it there.
       </p>
       <p id="warning">
         Note that this may restart some statistics and you're using it at your
         own risk.
       </p>
     </header>
-    <textarea name="textarea" rows="35" cols="35" readonly><?php require "../code/upgrades.php"; ?></textarea>
+    <textarea name="textarea" rows="35" cols="35" readonly>
+<?php require "../code/upgrades.php"; ?></textarea
+    >
     <?php require "../require/down-copy.php"; ?>
     <?php require "../require/buttons.php"; ?>
     <?php require "../require/footer.php"; ?>
--- page/upgrades.php
+++ page/upgrades.php
@@ -8,56 +8,100 @@
   <body>
     <header>
       <h1>Generate your Upgrades</h1>
-      <p id="title">Fill out the options and generate your customized JSON template code.</p>
+      <p id="title">
+        Fill out the options and generate your customized JSON template code.
+      </p>
     </header>
     <form method="get" action="../generator/upgrades.php">
       <fieldset>
         <div>
           <legend>Game data</legend>
-          <label>Jetpack:</label><br>
-          <input type="number" name="jetpack" required min="0" max="6">
-          <span class="required">*</span><br>
+          <label>Jetpack:</label><br />
+          <input type="number" name="jetpack" required min="0" max="6" />
+          <span class="required">*</span><br />
         </div>
         <div>
-          <label>Super Sneakers:</label><br>
-          <input type="number" name="superSneakers" required min="0" max="6">
-          <span class="required">*</span><br>
+          <label>Super Sneakers:</label><br />
+          <input type="number" name="superSneakers" required min="0" max="6" />
+          <span class="required">*</span><br />
         </div>
         <div>
-          <label>Magnet:</label><br>
-          <input type="number" name="magnet" required min="0" max="6">
-          <span class="required">*</span><br>
+          <label>Magnet:</label><br />
+          <input type="number" name="magnet" required min="0" max="6" />
+          <span class="required">*</span><br />
         </div>
         <div>
-          <label>Double Score:</label><br>
-          <input type="number" name="doubleScore" required min="0" max="6">
-          <span class="required">*</span><br>
+          <label>Double Score:</label><br />
+          <input type="number" name="doubleScore" required min="0" max="6" />
+          <span class="required">*</span><br />
         </div>
         <div>
           <label for="doubleCoins">Double Coins:</label>
           <label class="switch">
-            <input type="checkbox" id="doubleCoins">
+            <input type="checkbox" id="doubleCoins" />
             <span class="slider"></span>
           </label>
         </div>
         <div>
           <label for="doubleCoinsAmount">Double Coins Amount:</label>
-          <input type="number" name="doubleCoinsAmount" id="doubleCoinsAmount" required min="0" max="100" disabled>
+          <input
+            type="number"
+            name="doubleCoinsAmount"
+            id="doubleCoinsAmount"
+            min="0"
+            max="100"
+            required
+            disabled
+          />
           <span class="required">*</span>
         </div>
-        <script>
-          const doubleCoins = document.getElementById("doubleCoins");
-          const doubleCoinsAmount = document.getElementsByName("doubleCoinsAmount")[0];
 
-          doubleCoins.addEventListener("change", () => {
+        <script>
+          const doubleCoins = document.getElementById('doubleCoins');
+          const doubleCoinsAmount =
+            document.getElementsByName('doubleCoinsAmount')[0];
+
+          doubleCoins.addEventListener('change', () => {
             doubleCoinsAmount.disabled = !doubleCoins.checked;
           });
         </script>
+
+        <div>
+          <label for="tokenBoost">Token Boost:</label>
+          <label class="switch">
+            <input type="checkbox" id="tokenBoost" />
+            <span class="slider"></span>
+          </label>
+        </div>
+
+        <div>
+          <label for="tokenBoostAmount">Token Boost Amount:</label>
+          <input
+            type="number"
+            name="tokenBoostAmount"
+            id="tokenBoostAmount"
+            min="0"
+            max="100"
+            required
+            disabled
+          />
+          <span class="required">*</span>
+        </div>
+
+        <script>
+          const tokenBoost = document.getElementById('tokenBoost');
+          const tokenBoostAmount =
+            document.getElementsByName('tokenBoostAmount')[0];
+
+          tokenBoost.addEventListener('change', () => {
+            tokenBoostAmount.disabled = !tokenBoost.checked;
+          });
+        </script>
       </fieldset>
-      <br>
-      <input type="submit" class="btn btn-success">
+      <br />
+      <input type="submit" class="btn btn-success" />
     </form>
-    
+
     <?php require "../require/footer.php"; ?>
   </body>
 </html>
