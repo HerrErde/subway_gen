d14e3f4c08d184a4611d3fd7aa6a9c76715a1af3
Herr Erde <Natodamm@hotmail.com>
2023-06-15 00:41:44 +0200

    add analytics

--- require/connect.php
+++ require/connect.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<script defer data-domain="subway.herrerde.xyz" src="https://a.herrerde.xyz/js/script.js"></script>
+
 <script
   src="https://kit.fontawesome.com/ede9707ca2.js"
   crossorigin="anonymous"
