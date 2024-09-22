20aad9b5c087c6bbbb7aa510c14ec391b7998dcf
Herr Erde <Natodamm@hotmail.com>
2024-09-22 05:29:01 +0200

    add Frames

--- src/require/buttons.php
+++ src/require/buttons.php
@@ -13,6 +13,7 @@ if (!isset($activePage)) {
         ['url' => '/page/character.php', 'name' => 'Characters', 'active' => $activePage == "character"],
         ['url' => '/page/hoverboard.php', 'name' => 'Hoverboards', 'active' => $activePage == "hoverboard"],
         ['url' => '/page/portraits.php', 'name' => 'Portraits', 'active' => $activePage == "portraits"],
+        ['url' => '/page/frames.php', 'name' => 'Frames', 'active' => $activePage == "frames"],
         ['url' => '/page/badges.php', 'name' => 'Badges', 'active' => $activePage == "badges"],
         ['url' => '/page/upgrades.php', 'name' => 'Upgrades', 'active' => $activePage == "upgrades"],
         ['url' => '/page/toprun.php', 'name' => 'Top Run', 'active' => $activePage == "toprun"]
